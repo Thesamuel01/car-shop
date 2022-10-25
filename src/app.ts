d2d3d25010c373a1de30d2b36667e89866f1b45a
samuel santos <samaraujo28@gmail.com>
2022-10-25 20:54:16 -0300

    feat: implement post /cars route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import errorHandler from './middleware/error';
+import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+app.use(routes);
+app.use(errorHandler);
+
 export default app;
-// Iniciando projeto
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,16 @@
 import { Router } from 'express';
 import 'express-async-errors';
 
+import CarController from './controllers/CarController';
+import CarModel from './models/Car';
+import CarService from './services/CarService';
+
 const routes = Router();
+
+const car = new CarModel();
+const carService = new CarService(car);
+const carController = new CarController(carService);
+
+routes.post('/cars', carController.create);
 
 export default routes;
