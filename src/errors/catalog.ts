14bf21f3571af09f4822aedb6bb0d264e67c525a
samuel santos <samaraujo28@gmail.com>
2022-10-25 17:15:46 -0300

    feat: implement error handler middleware

---
+++ src/errors/catalog.ts
@@ -0,0 +1,12 @@
+export enum ErrorTypes {}
+
+type ErrorResponseObject = { 
+  message: string;
+  httpStatus: number
+};
+
+export type ErrorCatalog = {
+  [key in ErrorTypes]: ErrorResponseObject
+};
+
+export const errorCatalog: ErrorCatalog = {};
